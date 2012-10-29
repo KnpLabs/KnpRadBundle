97d5e599d3d94d21c01f7bca321351f46b98159c
everzet <ever.zet@gmail.com>
2012-10-29 16:10:41 +0100

    Use base bundle

--- AppBundle/Bundle.php
+++ AppBundle/Bundle.php
@@ -6,30 +6,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class Bundle extends BaseBundle
 {
-    private $path;
-
-    public function __construct($path, $name = 'App')
-    {
-        $this->path = $path;
-        $this->name = $name;
-    }
-
-    public function getPath()
-    {
-        return $this->path;
-    }
-
-    public function getNamespace()
-    {
-        return $this->name;
-    }
-
     public function getContainerExtension()
     {
         if ($extension = parent::getContainerExtension()) {
             return $extension;
         }
 
-        return $this->extension = new ContainerExtension($this->path);
+        return $this->extension = new ContainerExtension(__DIR__);
     }
 }
