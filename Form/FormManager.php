a934e91f728c045a37e1634474b1f76dc6340b62
Gildas Quéméner <gildas.quemener@knplabs.com>
2012-10-30 13:46:06 +0100

    Introduce the form manager

---
+++ Form/FormManager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Knp\RadBundle\Form;
+
+class FormManager
+{
+    private $factory;
+
+    public function __construct($factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createFormFor($entity, $data = null, array $options = array())
+    {
+        return $this->factory->create($this->computeFormType($entity), $data, $options);
+    }
+
+    /**
+     * @throw \InvalidArgumentException when $entity is null
+     */
+    private function computeFormType($entity)
+    {
+        if (!is_object($entity)) {
+            throw new \InvalidArgumentException(sprintf('Expected object, got %s', gettype($entity)));
+        }
+
+        $class = get_class($entity);
+        $formClass = $this->getFormType($class);
+
+        return new $formClass();
+    }
+
+    private function getFormType($entityClass)
+    {
+        $arr = explode('\\', $entityClass);
+        $formClass = sprintf('App\Form\%sType', end($arr));
+
+        if (!class_exists($formClass)) {
+            if (false !== $parentClass = get_parent_class($entityClass)) {
+                $formClass = $this->getFormType($parentClass);
+            } else {
+                throw new \Exception(sprintf('Couldn\'t find a form type associated with %s', $entityClass));
+            }
+        }
+
+        return $formClass;
+    }
+}
