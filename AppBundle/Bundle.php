c75f757a8d542323ea8e21ce504026a718e340cc
Antoine Hérault <antoine.herault@gmail.com>
2012-10-31 11:03:45 +0100

    Automatically register entity repos into DIC

--- AppBundle/Bundle.php
+++ AppBundle/Bundle.php
@@ -3,9 +3,16 @@
 namespace Knp\RadBundle\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Knp\RadBundle\DependencyInjection\Compiler\RegisterDoctrineRepositoriesPass;
 
 class Bundle extends BaseBundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new RegisterDoctrineRepositoriesPass($this));
+    }
+
     public function getContainerExtension()
     {
         if ($extension = parent::getContainerExtension()) {
