c7719056a82a67a964f4abe33c2ab7587d71a27c
Antoine Hérault <antoine.herault@gmail.com>
2012-10-31 17:38:07 +0100

    Make entity repository services public

--- DependencyInjection/Definition/DoctrineRepositoryFactory.php
+++ DependencyInjection/Definition/DoctrineRepositoryFactory.php
@@ -9,7 +9,6 @@ class DoctrineRepositoryFactory
     public function createDefinition($className)
     {
         $definition = new Definition();
-        $definition->setPublic(false);
         $definition->setClass('Doctrine\Common\Persistence\ObjectRepository');
         $definition->setFactoryService('doctrine');
         $definition->setFactoryMethod('getRepository');
