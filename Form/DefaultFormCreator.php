e41dafbc8652ec211d830575f115ba59e06bf78a
Gildas Quéméner <gildas.quemener@knplabs.com>
2012-10-31 18:10:36 +0100

    Implemented Form Manager

--- Form/DefaultFormCreator.php
+++ Form/DefaultFormCreator.php
@@ -4,29 +4,37 @@ namespace Knp\RadBundle\Form;
 
 use Symfony\Component\Form\FormFactoryInterface;
 
-class DefaultFormCreator
+class DefaultFormCreator implements FormCreatorInterface
 {
+    private $fetcher;
     private $factory;
 
-    public function __construct(FormFactoryInterface $factory)
+    public function __construct(ClassMetadataFetcher $fetcher = null, FormFactoryInterface $factory)
     {
+        $this->fetcher = $fetcher ?: new ClassMetadataFetcher;
         $this->factory = $factory;
     }
 
-    public function buildFormForObject($entity, $options)
+    public function create($object, $purpose = null, array $options = array())
     {
-        $builder = $this->factory->createBuilder('form', $entity, $options);
-        $class = new \ReflectionClass(get_class($entity));
+        $builder = $this->factory->createBuilder('form', $object, $options);
 
-        foreach ($class->getMethods() as $method) {
-            if (0 === strpos($method->name, 'get')) {
-                $propertyName = strtolower(substr($method->name, 3));
-                if ($class->hasMethod('set'.ucfirst($propertyName))) {
-                    $builder->add($propertyName);
-                }
+        foreach ($this->fetcher->getMethods($object) as $method) {
+            if (0 === strpos($method, 'get')) {
+                $this->addField($builder, $object, strtolower(substr($method, 3)));
+            }
+            if (0 === strpos($method, 'is')) {
+                $this->addField($builder, $object, strtolower(substr($method, 2)));
             }
         }
 
         return $builder->getForm();
+    }
+
+    private function addField($builder, $object, $propertyName)
+    {
+        if ($this->fetcher->hasMethod($object, 'set'.ucfirst($propertyName))) {
+            $builder->add($propertyName);
+        }
     }
 }
--- Form/FormManager.php
+++ Form/FormManager.php
@@ -2,52 +2,27 @@
 
 namespace Knp\RadBundle\Form;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormFactoryInterface;
-
 class FormManager
 {
-    private $factory;
-    private $formCreator;
+    private $creators = array();
 
-    public function __construct(FormFactoryInterface $factory, DefaultFormCreator $formCreator)
+    public function createObjectForm($object, $purpose = null, array $options = array())
     {
-        $this->factory     = $factory;
-        $this->formCreator = $formCreator;
-    }
-
-    public function createFormFor($data, $name = null, array $options = array())
-    {
-        if (null !== $name) {
-            // if a FormType is provided, we use it
-            //TODO Check that FormType provided is compatible with $data class
-            return $this->factory->create($this->getFormType($data, $name), $data, $options);
-        }
-
-        // We look for a corresponding FormType
-        if (null === $formType = $this->getDefaultFormType($data)) {
-            // if no form type is found, we create one by looking $data properties
-
-            return $this->formCreator->buildFormForObject($data, $options);
-        }
-
-        // If one is found, we use it
-        return $this->factory->create($formType, $data, $options);
-    }
-
-    private function getFormType($entity, $name = null)
-    {
-        $entityClass = is_object($entity) ? get_class($entity) : $entity;
-        $arr = explode('\\', $entityClass);
-        $formClass = sprintf('App\Form\%s%sType', ucfirst($name), end($arr));
-        if (!class_exists($formClass)) {
-            if (false !== $parentClass = get_parent_class($entity)) {
-                $formClass = $this->getFormType($parentClass);
-            } else {
-                return;
+        foreach ($this->creators as $creator) {
+            if ($form = $creator->create($object, $purpose, $options)) {
+                return $form;
             }
         }
+        throw new \RuntimeException(sprintf('No creator fitted your need!!!'));
+    }
 
-        return $formClass;
+    public function registerCreator(FormCreatorInterface $creator)
+    {
+        $this->creators[] = $creator;
+    }
+
+    public function getCreators()
+    {
+        return $this->creators;
     }
 }
