0949da9d02638b7041922675ed9714c5ab2afd55
Antoine Hérault <antoine.herault@gmail.com>
2012-11-02 14:34:42 +0100

    Inject container to container aware twig extensions

--- DependencyInjection/Compiler/TwigExtensionServiceFactory.php
+++ DependencyInjection/Compiler/TwigExtensionServiceFactory.php
@@ -3,13 +3,33 @@
 namespace Knp\RadBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Definition;
+use Knp\RadBundle\Reflection\ReflectionFactory;
+use Knp\RadBundle\DependencyInjection\ReferenceFactory;
 
 class TwigExtensionServiceFactory
 {
+    const CONTAINER_AWARE_INTERFACE = 'Symfony\Component\DependencyInjection\ContainerAwareInterface';
+    const CONTAINER_SERVICE_ID = 'service_container';
+
+    private $reflectionFactory;
+
+    public function __construct(ReflectionFactory $reflectionFactory = null, ReferenceFactory $referenceFactory = null)
+    {
+        $this->reflectionFactory = $reflectionFactory ?: new ReflectionFactory();
+        $this->referenceFactory = $referenceFactory ?: new ReferenceFactory();
+    }
+
     public function createDefinition($className)
     {
         $definition = new Definition($className);
         $definition->setPublic(false);
+
+        $reflClass = $this->reflectionFactory->createReflectionClass($className);
+
+        if ($reflClass->implementsInterface(static::CONTAINER_AWARE_INTERFACE)) {
+            $containerRef = $this->referenceFactory->createReference(static::CONTAINER_SERVICE_ID);
+            $definition->addMethodCall('setContainer', array($containerRef));
+        }
 
         return $definition;
     }
