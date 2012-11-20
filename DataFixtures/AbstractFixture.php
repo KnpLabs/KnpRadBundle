3c41ced766a49e4c3176a2dfb4aa305bb7f5026a
Gildas Quéméner <gildas.quemener@knplabs.com>
2012-11-20 14:18:13 +0100

    Added the container in the rad abstract fixtures

--- DataFixtures/AbstractFixture.php
+++ DataFixtures/AbstractFixture.php
@@ -4,11 +4,20 @@ namespace Knp\RadBundle\DataFixtures;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\AbstractFixture as BaseAbstractFixture;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-abstract class AbstractFixture extends BaseAbstractFixture
+abstract class AbstractFixture extends BaseAbstractFixture implements ContainerAwareInterface
 {
+    protected $container;
+
     public function createObjectFactory(ObjectManager $manager, $className)
     {
         return new ObjectFactory($this->referenceRepository, new ReferenceManipulator($this->referenceRepository), $manager, $className);
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 }
