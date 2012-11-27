fd8602a90c2547b05dfbadd0e1eb2c59776c2be1
Gildas Quéméner <gildas.quemener@knplabs.com>
2012-11-27 17:29:45 +0100

    Added a parameters.yml.dist handler for composer

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Composer;
+
+use Sensio\Bundle\DistributionBundle\Composer\ScriptHandlern as BaseScriptHandler;
+
+class ScriptHandler extends BaseScriptHandler
+{
+    public static function installParametersFile($event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        if (!is_dir($appDir)) {
+            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not install the parameters.yml file.'.PHP_EOL;
+
+            return;
+        }
+
+        if (is_file($appDir.'/config/parameters.yml.dist') && !is_file($appDir.'/config/parameters.yml')) {
+            copy($appDir.'/config/parameters.yml.dist', $appDir.'/config/parameters.yml');
+        }
+    }
+}
