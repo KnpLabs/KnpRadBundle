2da6593453afeff1cc58807b51ad2dc1706630dd
Gildas Quéméner <gildas.quemener@knplabs.com>
2013-01-15 10:40:26 +0100

    Use the service id generator in the repository registration compiler pass

--- spec/DependencyInjection/ServiceIdGenerator.php
+++ spec/DependencyInjection/ServiceIdGenerator.php
@@ -13,14 +13,18 @@ class ServiceIdGenerator extends ObjectBehavior
     function let($bundle, $extension)
     {
         $bundle->getNamespace()->willReturn('Knp\BlogBundle');
-
         $extension->getAlias()->willReturn('knp_blog');
-    }
-
-    function it_should_generate_a_service_id_for_a_bundle_class($bundle, $extension)
-    {
         $bundle->getContainerExtension()->willReturn($extension);
 
+    }
+
+    function it_should_generate_a_service_id_for_a_bundle_class($bundle)
+    {
         $this->generateForBundleClass($bundle, 'Knp\BlogBundle\Rating\ArticleRater')->shouldReturn('knp_blog.rating.article_rater');
+    }
+
+    function it_should_generate_a_service_id_with_a_suffix_if_provided($bundle)
+    {
+        $this->generateForBundleClass($bundle, 'Knp\BlogBundle\Entity\BlogPost', 'repository')->shouldReturn('knp_blog.entity.blog_post_repository');
     }
 }
