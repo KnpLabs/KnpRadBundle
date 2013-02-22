857736f12716287f282013f5e964ee056d10bfa2
Gildas Quéméner <gildas.quemener@knplabs.com>
2013-02-22 14:16:29 +0100

    Revert "Restore usage of class finder in doctrine repositories registration pass"
    This reverts commit 14b81e4652e0979b7ed4917d761ee03810dbf151.

--- AppBundle/Bundle.php
+++ AppBundle/Bundle.php
@@ -4,6 +4,7 @@ namespace Knp\RadBundle\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Knp\RadBundle\DependencyInjection\Compiler;
 
 class Bundle extends BaseBundle
@@ -12,7 +13,7 @@ class Bundle extends BaseBundle
 
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(new Compiler\RegisterDoctrineRepositoriesPass($this));
+        $container->addCompilerPass(new Compiler\RegisterDoctrineRepositoriesPass($this), PassConfig::TYPE_OPTIMIZE);
         $container->addCompilerPass(new Compiler\RegisterTwigExtensionsPass($this));
         $container->addCompilerPass(new Compiler\RegisterSecurityVotersPass($this));
         $container->addCompilerPass(new Compiler\RegisterAppBundlePass($this));
--- DependencyInjection/Compiler/RegisterDoctrineRepositoriesPass.php
+++ DependencyInjection/Compiler/RegisterDoctrineRepositoriesPass.php
@@ -4,7 +4,6 @@ namespace Knp\RadBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Knp\RadBundle\Finder\ClassFinder;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\DependencyInjection\Container;
 use Knp\RadBundle\DependencyInjection\Definition\DoctrineRepositoryFactory;
@@ -13,14 +12,12 @@ use Knp\RadBundle\DependencyInjection\ServiceIdGenerator;
 class RegisterDoctrineRepositoriesPass implements CompilerPassInterface
 {
     private $bundle;
-    private $classFinder;
     private $definitionFactory;
     private $serviceIdGenerator;
 
-    public function __construct(BundleInterface $bundle, ClassFinder $classFinder = null, DoctrineRepositoryFactory $definitionFactory = null, ServiceIdGenerator $serviceIdGenerator = null)
+    public function __construct(BundleInterface $bundle, DoctrineRepositoryFactory $definitionFactory = null, ServiceIdGenerator $serviceIdGenerator = null)
     {
         $this->bundle             = $bundle;
-        $this->classFinder        = $classFinder ?: new ClassFinder();
         $this->definitionFactory  = $definitionFactory ?: new DoctrineRepositoryFactory();
         $this->serviceIdGenerator = $serviceIdGenerator ?: new ServiceIdGenerator();
     }
@@ -30,21 +27,24 @@ class RegisterDoctrineRepositoriesPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $directory = $this->bundle->getPath().'/Entity';
         $namespace = $this->bundle->getNamespace().'\Entity';
 
-        $classes = $this->classFinder->findClassesMatching($directory, $namespace, '(?<!Repository)$');
+        if ($container->has('doctrine.orm.entity_manager')) {
+            $classes = $container->get('doctrine.orm.entity_manager')->getConfiguration()->getMetadataDriverImpl()->getAllClassNames();
 
-        foreach ($classes as $class) {
-            $id = $this->serviceIdGenerator->generateForBundleClass($this->bundle, $class, 'repository');
+            foreach ($classes as $class) {
+                if (strpos($class, $this->bundle->getNamespace()) === 0) {
+                    $id = $this->serviceIdGenerator->generateForBundleClass($this->bundle, $class, 'repository');
 
-            if ($container->hasDefinition($id)) {
-                continue;
+                    if ($container->hasDefinition($id)) {
+                        continue;
+                    }
+
+                    $def = $this->definitionFactory->createDefinition($class);
+
+                    $container->setDefinition($id, $def);
+                }
             }
-
-            $def = $this->definitionFactory->createDefinition($class);
-
-            $container->setDefinition($id, $def);
         }
     }
 }
