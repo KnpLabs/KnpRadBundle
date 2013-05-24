9d21f72c6680a4678cbefb10562c0bbe35e851f7
davidjegat <david.jegat@gmail.com>
2013-05-24 11:43:37 +0200

    ConfigurableBundleInterface extends now from the standard BundleInterface

--- AppBundle/ConfigurableBundleInterface.php
+++ AppBundle/ConfigurableBundleInterface.php
@@ -5,8 +5,9 @@ namespace Knp\RadBundle\AppBundle;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\Definition\Builder\NodeParentInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
-interface ConfigurableBundleInterface 
+interface ConfigurableBundleInterface extends BundleInterface
 {
     /**
      * Configure the root node of a given application using the
