f7da2b39085d5b87876a0a98fa16aca23d084b83
Klein Florian <florian.klein@free.fr>
2013-08-19 22:09:04 +0200

    add _check_csrf option to enable verification

--- EventListener/CsrfListener.php
+++ EventListener/CsrfListener.php
@@ -19,15 +19,20 @@ class CsrfListener
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
-        if (!$request->request->has('_link_token')) {
+        if (false === $request->attributes->get('_check_csrf', false)) {
             return;
+        }
+        if (!$request->request->has('_link_token')) {
+            throw new \InvalidArgumentException(
+                'The CSRF token verification is activated but you did not send a token. Please submit a request with a valid csrf token.'
+            );
         }
 
         $token = $request->request->get('_link_token');
 
         if (!$this->csrfProvider->isCsrfTokenValid($this->intention, $token)) {
             throw new \InvalidArgumentException(
-                'The CSRF token is invalid. Please try to resubmit the form.'
+                'The CSRF token is invalid. Please submit a request with a valid csrf token.'
             );
         }
     }
